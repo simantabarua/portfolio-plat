e2471d46e188df0bd2e554852e8d4b9f5fa0cf82
simanta <simanta.barua@goama.com>
2025-02-13 01:59:35 +0600

    fix bac

--- backendsvc/backendsvc/settings.py
+++ backendsvc/backendsvc/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "djangoprojdfsdectsdfdsdjsdfsdfsd34fangop34roject"
+SECRET_KEY = "django-insecure-yx9ppd(_aiahse4u8t)ky6*2$km(=i9y7_5ti^tgt!-3tb!f9#"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backendsvc/backendsvc/wsgi.py
+++ backendsvc/backendsvc/wsgi.py
@@ -8,11 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendsvc.settings')
+# Set the default settings module for the 'DJANGO_SETTINGS_MODULE' environment variable.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendsvc.backendsvc.settings')
 
+# Get the WSGI application for the project.
 application = get_wsgi_application()
 
+# Expose the application as a module-level variable named 'app'.
 app = application
